ee6ce132185cb5b6696a5ef136c1159ba07fef8d
nsanchezangulo <92123987+nsanchezangulo@users.noreply.github.com>
2021-12-15 17:11:19 -0500

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,4 +1,4 @@
-job ('ejemplo-job-DSL'){
+job ('ejemplo2-job-DSL'){
 	description(' Job DLS de ejemplo ')
   scm{
     git('https://github.com/nsanchezangulo/jenkins.job.parametrizado.git', 'main'){ node ->
