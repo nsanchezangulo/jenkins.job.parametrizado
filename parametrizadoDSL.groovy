996612b9d4054f0d7d5fe15983226a77bb27cb9f
nsanchezangulo <92123987+nsanchezangulo@users.noreply.github.com>
2021-12-15 17:10:35 -0500

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,40 +1,40 @@
-job('ejemplo2-job-DSL') {
-	description('Job DSL de ejemplo para el curso de Jenkins')
-  	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
-      		}
-    	} 
-  	parameters {
-   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
-      		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
-      		booleanParam('agente', false)
-    	}
-  	triggers {
-    		cron('H/7 * * * *')
-    	}
-  	steps {
-    		shell("bash jobscript.sh")
-    	}
-  	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
-      		slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
-        	}
-    	}
+job ('ejemplo-job-DSL'){
+	description(' Job DLS de ejemplo ')
+  scm{
+    git('https://github.com/nsanchezangulo/jenkins.job.parametrizado.git', 'main'){ node ->
+      node / gitConfigName('nsanchezan')
+      node / gitConfigEmail('nsanchezan@gmail.com')
+     }
+  }
+  parameters {
+      stringParam('nombre', defaultValue='nsancheza', description = 'Parametro de cadena para el job boolean')
+      choiceParam ('planeta', ['venus','tierra','marte','pluton'])
+      booleanParam('agente', false)
+  }
+  triggers{
+      cron ('H/7 * * * *')
+  }
+  steps{
+      shell("bash jobscript.sh")
+  }
+  publishers{
+      mailer('nsanchezangulo@hotmail.com', true , true )
+      slackNotifier {
+      notifyAborted(true)
+      notifyEveryFailure(true)
+      notifyNotBuilt(false)
+      notifyUnstable(false)
+      notifyBackToNormal(true)
+      notifySuccess(false)
+      notifyRepeatedFailure(false)
+      startNotification(false)
+      includeTestSummary(false)
+      includeCustomMessage(false)
+      customMessage(null)
+      sendAs(null)
+      commitInfoChoice('NONE')
+      teamDomain(null)
+      authToken(null)
+    }
+  }
 }
